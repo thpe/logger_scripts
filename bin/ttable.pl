8f223f722462ff742be9b1c34a71fd3967e83c0f
Thomas Petig <thomas@petig.eu>
2015-01-02 19:58:19 +0100

    fixed new year bug

--- bin/ttable.pl
+++ bin/ttable.pl
@@ -65,8 +65,8 @@ while (<>) {
     ++$start_year;
 }
 for (; $start_year <= $year; ++$start_year) {
-    print FILE $line;
-    $line = "\n";
+    print FILE $line."\n";
+    $line = "";
 }
 $csv->parse($line);
 @columns = $csv->fields();
